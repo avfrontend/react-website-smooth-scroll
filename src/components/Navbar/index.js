b22a07791dab147213e482bda6cd618c1250f079
avfrontend <avraam.kastrappis@gmail.com>
2021-08-18 19:11:10 +0300

    react website with smooth scroll

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Nav, NavbarContainer, NavLogo} from './NavbarElements'
+
+function Navbar() {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">dolla</NavLogo>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
+
